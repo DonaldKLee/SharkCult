f9cd583c46adb2314f77a6e2ae2eb193f87ba609
Donald Lee <51865552+DonaldKLee@users.noreply.github.com>
2021-08-27 17:01:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, url_for, redirect
+import random
+
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
+
+# Prevents cache from using the old css file, makes it use the updated one
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+# ^ ^ ^
+
+@app.route('/')  # Home page
+def home():
+	return render_template("index.html")
+
+# For repl hosting
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000),  # Randomly select the port the machine hosts on.
+        debug=True # Updates site when new changes are made
+    )
