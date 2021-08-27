64e0f3387c002180c356d435cece28122e0bab4e
Donald Lee <51865552+DonaldKLee@users.noreply.github.com>
2021-08-27 20:13:58 +0000

    :)

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -1,5 +1,4 @@
 function shark_clicked() {
-    
     var hearts = document.getElementById("hearts");
 
     // If user's hearts are not below or equal to 0
@@ -13,6 +12,7 @@ function hearts_used() {
     var hearts = document.getElementById("hearts");
 
     // If user's hearts are not below or equal to 0
+
     if (!(hearts.innerHTML <= 0)) {
         hearts.innerHTML = --hearts.innerHTML;
     }
@@ -21,3 +21,29 @@ function hearts_used() {
        console.log("Game over!")
    }
 }
+
+function spawn_shark() {
+    // make this 
+    // <img src="static/images/morningshark.png" class="shark" onclick="shark_clicked()" draggable="false" unselectable="on"/>
+
+    var game_container = document.getElementById("game_container")
+
+    var shark = document.createElement("img");
+    shark.src = "static/images/morningshark.png";  
+
+    shark.classList.add("shark");
+
+    //shark.setAttribute("onclick","shark_clicked()");
+
+    shark.onclick = function() {
+        
+    };
+
+    shark.setAttribute('draggable', false);
+    shark.setAttribute('unselectable', "on");
+
+    game_container.appendChild(shark);
+
+}
+
+setInterval(spawn_shark, 3000)
