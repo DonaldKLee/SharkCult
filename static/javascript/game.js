18d24cdaa2434d4bd0f9fcf0f35361ffd36cf569
Donald Lee <51865552+DonaldKLee@users.noreply.github.com>
2021-08-28 23:20:38 +0000

    Worked on front-end

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -9,6 +9,11 @@ function hearts_used() {
 
    else {
        console.log("Game over!")
+
+       var game_over_sign = document.getElementById("gameover");
+       game_over_sign.style.display = "block";
+       game_over_sign.classList.add("show_gameover");
+
    }
 }
 
@@ -39,19 +44,47 @@ function spawn_shark() {
     shark.classList.add("shark");
 
     shark_name = shark_type
-    //shark_points = shark_type[1]
 
-    shark_stats = Math.floor(Math.random() * (5)) + 1;
-    shark_speed = 4.5/shark_stats;
+    shark_points = Math.floor(Math.random() * (5)) + 1;
+    if (shark_points === 5) {
+        shark_speed = 0.1;
+    }
+
+    if (shark_points === 4) {
+        shark_speed = 0.3;
+    }
+
+    if (shark_points === 3) {
+        shark_speed = 0.5;
+    }
+
+    if (shark_points === 2) {
+        shark_speed = 0.8;
+    }
+
+    else {
+        shark_speed = 1;
+    }
 
     shark.setAttribute("style", "animation-duration:" + shark_speed + "s;");
 
-    if (Math.random() < 0.3) {
+    if (Math.random() < 0.3) { // 30% chance of a backwards shark
         shark.setAttribute("style", "animation-duration:" + shark_speed + "s; transform: scaleX(-0.5) scaleY(0.5);");
     }
 
-    shark.setAttribute('alt', shark_stats); // Shark points
+    // Give it random keyframes
 
+
+    // Or just a different left and top
+
+    // Maybe have one have a right keyframes (Make another keyframes)
+
+
+
+
+    shark.setAttribute('alt', shark_points); // Shark points
+
+    // If the shark isn't captured
     shark.addEventListener("animationend", function() {
         if (!(this.alt === "captured_shark")) {
             this.remove();
@@ -61,19 +94,30 @@ function spawn_shark() {
     shark.onclick = function() {
         var hearts = document.getElementById("hearts");
 
-        // If user's hearts are not below or equal to 0
-        if (!(hearts.innerHTML <= 0)) {
+        // If user's hearts are not below or equal to 0 and the shark is wild
+        if (!(hearts.innerHTML <= 0) && !(this.alt === "captured_shark")) {
             shark_points = this.alt;
             var score = document.getElementById("score");
-            new_score = parseInt(score.innerHTML) + parseInt(shark_stats);
+            new_score = parseInt(score.innerHTML) + parseInt(shark_points);
             console.log(shark_type)
 
             score.innerHTML = new_score;
             hearts.innerHTML = ++hearts.innerHTML; // Doesn't lose heart if also clicks
+            
             // Make it stay and move around
             shark.setAttribute('alt', "captured_shark"); // Shark points
 
-            //this.remove();
+
+            shark.animate([
+                // keyframes
+                { transform: 'scale(0.25)', left: '' + (Math.floor(Math.random() * (10) ) + 0) + '%' },
+                { transform: 'scale(0.25)', left: '' + (Math.floor(Math.random() * (70) ) + 60) + '%' }
+            ], {
+                // timing options
+                duration: Math.floor(Math.random() * (6000) ) + 5000,
+                iterations: Infinity,
+                direction: "alternate"
+            });
         }
     };
 
@@ -85,7 +129,6 @@ function spawn_shark() {
 }
 
 // Spawns a shark every X ms (X is between 1000 to 5000)
-setInterval(spawn_shark, Math.floor(Math.random() * (5000 - 1000 + 1000) ) + 1000);
+setInterval(spawn_shark, Math.floor(Math.random() * (5000) ) + 1000);
 
-// Use JS to set animation duration
 
